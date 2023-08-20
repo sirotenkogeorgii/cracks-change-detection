eae72bb585b809cef39c1909019b7ab19f591c8c
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-08-20 19:27:07 +0200

    add train mode of the model

--- defects_detection.py
+++ defects_detection.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from image_processing import affine_transformation, crop_patches, filter_patches, histogram_equalizing
+from image_processing import affine_transformation, crop_patches, histogram_equalizing, overlap_patches
 from cd_unet import CDUnet, UpSamplingBlock, ConvBlock
 import torchvision.transforms.functional as TF
 from helpers import reconstruct_image
@@ -11,19 +11,21 @@ import torch
 import cv2
 import os
 
-# TODO: Make inference with overlapping
 # TODO: Tune thresholds for unet
 
-# python3 defects_detection.py --input=examples/example_data/images/10_bez_crop.jpg --ref=examples/example_data/images/10_1_crop.jpg --diff --with_corners --grayscale
+# python3 defects_detection.py --input=/Users/georgiisirotenko/Downloads/2_bez_popisu_crop.jpg --ref=/Users/georgiisirotenko/Downloads/2_2_crop.jpg --colored --diff
+# python3 defects_detection.py --input=examples/example_data/images/10_bez_crop.jpg --ref=examples/example_data/images/10_1_crop.jpg --diff --grayscale
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", type=str, help="Images path.", required=True)
 parser.add_argument("--ref", type=str, help="Masks path.", required=True)
 parser.add_argument("--target_dir", default="./defects_output", type=str, help="Directory to save the results.")
 parser.add_argument("--model_path", default="./weights/segmentation/model_weights_60_norm.pth", type=str, help="Model path.")
+# parser.add_argument("--model_path", default="/Users/georgiisirotenko/Downloads/model_weights_60_eff_norm-2.pth", type=str, help="Model path.")
 parser.add_argument("--by_patches", action="store_true", help="Return patches.")
 parser.add_argument("--diff", action="store_true", help="Perform difference of predictions.")
-parser.add_argument("--with_corners", action="store_true", help="Process corners of images.")
 parser.add_argument("--colored", action="store_true", help="Color result.")
+parser.add_argument("--normalize", action="store_true", help="Normalize inputs.")
+parser.add_argument("--overlap", action="store_true", help="Perform prediction with overlapping.")
 
 
 def main(args: argparse.Namespace) -> None:
@@ -38,12 +40,8 @@ def main(args: argparse.Namespace) -> None:
     resized2 = cv2.resize(input2, (512 * 5, 512 * 5), interpolation=cv2.INTER_AREA)
     resized1 = affine_transformation(resized1, resized2, knn_neighbors=10, display_keypoints=False)
 
-    mask = np.ones((5,5))
-    if not args.with_corners:
-        for i in [0, 4]: mask[i, 0], mask[i, 4] = 0, 0
-
-    patches1 = filter_patches(crop_patches(resized1, 512), mask) 
-    patches2 = filter_patches(crop_patches(resized2, 512), mask) 
+    patches1 = crop_patches(resized1, 512, stride=512 if not args.overlap else 256)
+    patches2 = crop_patches(resized2, 512, stride=512 if not args.overlap else 256)
     equalized_patch_pairs = [histogram_equalizing(patch1, patch2, grayscale=True) for patch1, patch2 in zip(patches1, patches2)]
 
     def to_tensor_image(image: np.ndarray, normalize: bool = False) -> torch.Tensor:
@@ -51,27 +49,32 @@ def main(args: argparse.Namespace) -> None:
         if normalize: image = TF.normalize(image, mean=(0.485), std=(0.229)) 
         return image.float()
     
+    def overlap_and_crop(patches: list[np.ndarray]) -> np.ndarray:
+        overlapped_patches = overlap_patches([2560, 2560], 256, patches)
+        return crop_patches(overlapped_patches, 512, 512)
+
     model = torch.load(args.model_path, map_location=device).to(device)
-    
+    # model.eval() # NOTE: eval mode changes the result a lot. (maybe due to the batchnorm)
+    model.train(True)
+
     result_patches = []
-    if args.diff: 
-        images_preds1 = [model(to_tensor_image(image[0])[None, ...].to(device))[0][0].detach().numpy() for image in equalized_patch_pairs] 
-        images_preds2 = [model(to_tensor_image(image[1])[None, ...].to(device))[0][0].detach().numpy() for image in equalized_patch_pairs] 
+    if args.diff:       
+        
+        images_preds1 = [model(to_tensor_image(image[0], normalize=args.normalize)[None, ...].to(device))[0][0].detach().numpy() for image in equalized_patch_pairs] 
+        images_preds2 = [model(to_tensor_image(image[1], normalize=args.normalize)[None, ...].to(device))[0][0].detach().numpy() for image in equalized_patch_pairs] 
+        if args.overlap:
+            images_preds1 = overlap_and_crop(images_preds1)
+            images_preds2 = overlap_and_crop(images_preds2)
         result_patches = [abs(img1 - img2) for img1, img2 in zip(images_preds1, images_preds2)]
 
     else:
         for patch1, patch2 in equalized_patch_pairs:
-            _ = model(to_tensor_image(patch1)[None, ...].to(device))
-            result_patches.append(model(to_tensor_image(patch2)[None, ...].to(device), second_prop=True)[0][0].detach().numpy())
+            _ = model(to_tensor_image(patch1, normalize=args.normalize)[None, ...].to(device))
+            result_patches.append(model(to_tensor_image(patch2, normalize=args.normalize)[None, ...].to(device), second_prop=True)[0][0].detach().numpy())
+        if args.overlap: result_patches = overlap_and_crop(result_patches)
 
     os.makedirs(args.target_dir, exist_ok=True)
-
     if not args.by_patches:
-        if not args.with_corners:
-            result_patches.insert(0, np.zeros((512, 512)))
-            result_patches.insert(4, np.zeros((512, 512)))
-            result_patches.insert(20, np.zeros((512, 512)))
-            result_patches.insert(24, np.zeros((512, 512)))
         reconstructed_image = reconstruct_image(result_patches)
         if args.colored: reconstructed_image = plt.get_cmap('viridis')(reconstructed_image)[:, :, :3]
         Image.fromarray((reconstructed_image * 255).astype(np.uint8)).save(f"{args.target_dir}/change_detection.png")      
@@ -82,11 +85,6 @@ def main(args: argparse.Namespace) -> None:
             Image.fromarray((result_patch * 255).astype(np.uint8)).save(f"{args.target_dir}/{i}.png")      
 
 
-
 if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
-
-
-
-
