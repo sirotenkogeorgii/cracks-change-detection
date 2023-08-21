76c39f296e6f894ed56be534ce434ac47bb57df9
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-08-21 23:27:16 +0200

    add general ohem, dice and combined dice loss

--- loss_funstions.py
+++ loss_funstions.py
@@ -1,54 +1,45 @@
 import torch
+from torch import nn
+
+# class BCE_OHEM(torch.nn.BCELoss):                                                                                                                                                                             
+#     def __init__(self, ratio: float = 2/3) -> None:      
+#         super(BCE_OHEM, self).__init__(None, True)                                 
+#         self.ratio = ratio                                                                                                                                      
+#     def forward(self, preds: torch.Tensor, targets: torch.Tensor, ratio: float = None) -> torch.Tensor:                                           
+#         if ratio is not None: self.ratio = ratio  
+#         samples2take = int(self.ratio * preds.shape[0])
+#         preds_ = preds.clone()
+#         losses = torch.autograd.Variable(torch.zeros(preds_.shape[0]))
+#         for i in range(preds.shape[0]):
+#             losses[i] = torch.nn.functional.binary_cross_entropy(preds_[i], targets[i], reduce='sum')
+#         _, inds = torch.topk(losses, samples2take)
+#         return torch.mean(torch.index_select(losses, 0, inds))
 
 
-class BCE_OHEM(torch.nn.BCELoss):                                                                                                                                                                             
-    def __init__(self, ratio: float = 2/3) -> None:      
-        super(BCE_OHEM, self).__init__(None, True)                                 
-        self.ratio = ratio                                                         
-                                                                                   
+class OHEM(nn.Module):                                                                                                                                                                             
+    def __init__(self, loss_function = torch.nn.functional.binary_cross_entropy, ratio: float = 2/3, ) -> None:                                      
+        self.loss_function = loss_function
+        self.ratio = ratio                                                                                                                                
     def forward(self, preds: torch.Tensor, targets: torch.Tensor, ratio: float = None) -> torch.Tensor:                                           
         if ratio is not None: self.ratio = ratio  
         samples2take = int(self.ratio * preds.shape[0])
-        losses = torch.Tensor(list(map(lambda x: torch.nn.functional.binary_cross_entropy(x[0], x[1], reduce='sum'), zip(preds, targets))))
+        preds_ = preds.clone()
+        losses = torch.autograd.Variable(torch.zeros(preds_.shape[0]))
+        for i in range(preds.shape[0]):
+            losses[i] = self.loss_function(preds_[i], targets[i])
         _, inds = torch.topk(losses, samples2take)
         return torch.mean(torch.index_select(losses, 0, inds))
 
 
-class DiceLoss(nn.Module):
-    def __init__(self):
-        super(DiceLoss, self).__init__()
-
-    def forward(self, preds: torch.Tensor, targets: torch.Tensor):
-
-        prediction_shape = list(preds.shape)
-        prediction = torch.reshape(preds >= 0.5, [prediction_shape[0], torch.prod(torch.Tensor(prediction_shape[1:]), dtype=torch.int32)])
-    
-        mask_shape = list(targets.shape)
-        mask = torch.reshape(mask >= 0.5, [mask_shape[0], torch.prod(torch.Tensor(mask_shape[1:]), dtype=torch.int32)])
-        intersection_mask = torch.logical_and(prediction, mask) 
-
-        intersection = torch.sum(intersection_mask.type(torch.FloatTensor))
-        total_sum = (prediction + mask).type(torch.FloatTensor)
-        dice = (2. * intersection) / total_sum
+def dice_loss(inputs, targets):   
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
         
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection)/(inputs.sum() + targets.sum())  
         return 1 - dice
 
-
-class DiceBCELoss(nn.Module):
-    def __init__(self):
-        super(DiceLoss, self).__init__()
-
-    def forward(self, preds: torch.Tensor, targets: torch.Tensor):
-
-        prediction_shape = list(preds.shape)
-        prediction = torch.reshape(preds >= 0.5, [prediction_shape[0], torch.prod(torch.Tensor(prediction_shape[1:]), dtype=torch.int32)])
-    
-        mask_shape = list(targets.shape)
-        mask = torch.reshape(mask >= 0.5, [mask_shape[0], torch.prod(torch.Tensor(mask_shape[1:]), dtype=torch.int32)])
-        intersection_mask = torch.logical_and(prediction, mask) 
-           
-        intersection = torch.sum(intersection_mask.type(torch.FloatTensor))
-        total_sum = (prediction + mask).type(torch.FloatTensor)
-        dice = (2. * intersection) / total_sum
-        
-        return 1 - dice
+def dice_bce_loss(preds: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        dice_loss_val = dice_loss(preds, targets)
+        bce_loss_val = torch.nn.functional.binary_cross_entropy(preds, targets)    
+        return dice_loss_val + bce_loss_val
