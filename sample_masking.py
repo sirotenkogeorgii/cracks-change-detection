ac112ec91d5fc3e26e8d4bde714e9e592c45ec76
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-08-25 18:49:08 +0200

    fix thresholding and median filter

--- sample_masking.py
+++ sample_masking.py
@@ -13,13 +13,14 @@ from sample_masking_helpers import make_mask, remove_holes_objects
 from ensemble import UnetEnsemble
 
 # TODO: tune thresholds when the the models will ready
+# python3 sample_masking.py --threshold=0.15
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--images_path", default="generated_images", type=str, help="Images path.")
 parser.add_argument("--masks_path", default="generated_masks", type=str, help="Masks path.")
 parser.add_argument("--model_path", default="weights/segmentation/model_weights_60_norm.pth", type=str, help="Model path.")
-parser.add_argument("--threshold", default=0.23, type=float, help="Threshold to make mask binary.")
-parser.add_argument("--kernel_size", default=1, type=int, help="Kernel size for median filter.")
+parser.add_argument("--threshold", default=0.20, type=float, help="Threshold to make mask binary.")
+parser.add_argument("--ellipse_size", default=3, type=int, help="Ellipse for median filter.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 
 
@@ -27,27 +28,20 @@ def main(args: argparse.Namespace) -> None:
     torch.manual_seed(args.seed)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
-    models_path = ["/Users/georgiisirotenko/python_scripts/cracks-change-detection/weights/segmentation/model_weights_60_eff_norm.pth"]
-    augmentations = [(augmentation_wrapper(TF.vflip), True), 
-                    (augmentation_wrapper(TF.hflip), True), 
-                    (augmentation_wrapper(TF.adjust_brightness, random.uniform(0.3, 1)), False),
-                    # (augmentation_wrapper(TF.adjust_hue, random.uniform(-0.5, 0.5)), False),
-                    # (augmentation_wrapper(TF.adjust_sharpness, random.randint(2, 10)), False),
-                    # (augmentation_wrapper(TF.adjust_contrast, random.uniform(-2, 2)), False)
-                    ]
+    models_path = "weights/masking"
+    augmentations = [(augmentation_wrapper(TF.vflip), True), (augmentation_wrapper(TF.hflip), True)]
 
     model = UnetEnsemble(models_path, device, tta_mode=True, ttas=augmentations).to(device)
 
     os.makedirs(args.masks_path, exist_ok=True)
     for filename in os.listdir(args.images_path):
         image = cv2.imread(f"{args.images_path}/{filename}", cv2.IMREAD_GRAYSCALE)
-        model_output = model(image, normalize=True).detach().numpy() #.detach().numpy()[0][0]
+        model_output = model(image, normalize=True).detach().numpy()
+
+        mask = make_mask(model_output, args.threshold, args.ellipse_size)#torch.where(torch.Tensor(model_output) < args.threshold, 0, 1).numpy().astype(np.uint8) * 255
+        mask = remove_holes_objects(mask)
         
-        # TODO: change hp
-        # mask = make_mask(model_output, args.threshold, args.kernel_size)
-
-        mask = remove_holes_objects((model_output * 255).astype(np.uint8))
-
+        # mask = remove_holes_objects((model_output * 255).astype(np.uint8))
         Image.fromarray(mask).save(f"{args.masks_path}/mask_{filename}")
 
 if __name__ == "__main__":
